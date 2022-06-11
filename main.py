8163aab92b0e1a24e0be45edda40c8dceca68300
Jtalaat <john.abdelmasseh@outlook.com>
2022-06-11 23:09:50 +0200

    day 1

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import imp
-from operator import le
+
 from turtle import Turtle,Screen
 import time
 from snake import Snake
@@ -10,35 +9,23 @@ screen = Screen()
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("John Snake Game")
-
 screen.tracer(0)
+
+
+snake = Snake()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.5)
+    snake.move()
 
-
-
-
-
-
-
-
-
-
-
-#To_do__2
-#Move the snake
-#To_do__3
-#control the snake
-#To_do__4
-#crete food model
-#To_do__5
-#score and speed
-#To_do__6
-#crete wall
-#To_do__7
-#die when eat your body
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,19 +1,36 @@
 from turtle import Turtle
 start_Possition = [(0,0),(-20,0),(-40,0)]
-
+step = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 class Snake :
 
     def __init__(self):
         self.segments = []
         self.createSnake()
+        self.head = self.segments[0]
     
     def move(self):
         for seg in range(len(self.segments)-1,0,-1):
             newX = self.segments[seg-1].xcor()
             newY = self.segments[seg-1].ycor()
             self.segments[seg].goto(newX,newY)
-        self.segments[0].forward(20)
+        self.segments[0].forward(step)
 
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
     def createSnake(self):
         for p in start_Possition:
