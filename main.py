85a15da79ad463fa7ec57feb1198328375330f2b
Jtalaat <john.abdelmasseh@outlook.com>
2022-06-11 12:52:20 +0200

    Create to_do_items

---
+++ main.py
@@ -0,0 +1,14 @@
+#To_do__1
+#Create Snke
+#To_do__2
+#Move the snake
+#To_do__3
+#control the snake
+#To_do__4
+#crete food model
+#To_do__5
+#score and speed
+#To_do__6
+#crete wall
+#To_do__7
+#die when eat your body
